858cd5a79aa5778081ef221322ad987d3c7bfcbe
ahmed3oun <ahmedoun199@gmail.com>
2023-12-04 21:52:09 +0100

    react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,16 @@ import { createContext, useState } from "react";
 // import { useEffect, useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  LazyRouteFunction,
+} from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Test from "./pages/test";
+
 // import TodoItems from "./components/TodoItems";
 // import Posts from "./components/Posts";
 /* import ConditionalComponent from "./components/ConditionalComponent";
@@ -55,7 +65,7 @@ function App() {
             setTheme($event.target.checked ? "dark" : "light");
           }}
         />
-        <Counter />
+
         {/* <Forms></Forms>
       ***********
       <Profile first_name={"Ahmed"} last_name="OUN"/>
@@ -66,6 +76,45 @@ function App() {
       ***********
       <ListItems></ListItems> */}
       </div>
+      <nav>
+        <ul className="nav-list">
+          <li>
+            <a rel="stylesheet" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a rel="stylesheet" href="/about">
+              About
+            </a>
+          </li>
+          <li>
+            <a rel="stylesheet" href="/test">
+              Test
+            </a>
+          </li>
+          <li>
+            <a rel="stylesheet" href="/counter">
+              Counter
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home title="Home"></Home>} />
+          {/* element={<About />} */}
+          <Route
+            path="about"
+            lazy={(): Promise<any> => import("./pages/about")}
+          >
+            <Route index element={<About />} />
+            <Route path=":number" element={<About />} />
+          </Route>
+          <Route path="counter" element={<Counter />} />
+          <Route path="test" element={<Test />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeContext.Provider>
   );
 }
